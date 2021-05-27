c43839069a2d88119fe313904088ea84b19b85ef
laing raven <laingraven.java@gmail.com>
2021-05-28 00:34:39 +0700

    first commit

---
+++ part 1/search-suggestion/src/swing/EventClick.java
@@ -0,0 +1,10 @@
+package swing;
+
+import java.awt.Component;
+
+public interface EventClick {
+
+    public void itemClick(DataSearch data);
+
+    public void itemRemove(Component com, DataSearch data);
+}
